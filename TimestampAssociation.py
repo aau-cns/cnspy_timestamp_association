d1c20ef4bb1b271dbde8470daa63b73aa9e6cdfb
rojung <roland.jung@aau.at>
2020-09-09 11:34:49 +0200

    [DOC] header added

--- TimestampAssociation.py
+++ TimestampAssociation.py
@@ -2,6 +2,7 @@
 # Software License Agreement (GNU GPLv3  License)
 #
 # Copyright (c) 2020, Roland Jung (roland.jung@aau.at) , AAU, KPK, NAV
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
