73c7551b9177b06d66d4172d4f9f5976f7915da8
jungr-ait <roland.jung@aau.at>
2021-03-15 13:09:10 +0100

    [TASK] unittest folder created

--- timestamp_association/TimestampAssociation.py
+++ timestamp_association/TimestampAssociation.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-# Requirements:
-# sudo pip install numpy pandas
 ########################################################################################################################
 import numpy as np
 
@@ -71,60 +69,3 @@ class TimestampAssociation:
             return idx_1, idx_2, vec_1_matched, vec_2_matched
 
 
-########################################################################################################################
-#################################################### T E S T ###########################################################
-########################################################################################################################
-import unittest
-import time
-import csv
-from csv2dataframe.TimestampCSV2DataFrame import TimestampCSV2DataFrame
-
-
-class TimestampAssociation_Test(unittest.TestCase):
-    start_time = None
-
-    def tuple_list_2_csv(self, d, fn):
-        with open(fn, 'w') as f:
-            writer = csv.writer(f, lineterminator='\n')
-            writer.writerow(['idx1', 'idx2'])
-            for row in d:
-                writer.writerow(row)
-
-    def load_data(self):
-        df_t_est = TimestampCSV2DataFrame(
-            fn='./sample_data/t_est.csv')
-        df_t_gt = TimestampCSV2DataFrame(
-            fn='./sample_data/t_gt.csv')
-
-        return df_t_est, df_t_gt
-
-    def start(self):
-        self.start_time = time.time()
-
-    def stop(self):
-        print("Process time: " + str((time.time() - self.start_time)))
-
-    def test_load_from_CSV(self):
-        df_t_est, df_t_gt = self.load_data()
-        self.assertTrue(len(df_t_est.get_t_vec()) > 0)
-        self.assertTrue(len(df_t_gt.get_t_vec()) > 0)
-
-    def test_associate_1(self):
-        df_t_est, df_t_gt = self.load_data()
-
-        t_est = df_t_est.get_t_vec()
-        t_gt = df_t_gt.get_t_vec()
-
-        self.start()
-        idx_est, idx_gt, t_est_matched, t_gt_matched = TimestampAssociation.associate_timestamps(
-            t_est,
-            t_gt)
-
-        matches2 = zip(idx_est, idx_gt)
-        self.stop()
-        # takes: 0.5sec
-        self.tuple_list_2_csv(d=matches2, fn='./results/matches.txt')
-
-
-if __name__ == "__main__":
-    unittest.main()
